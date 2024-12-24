7aab15303ff2c9f91e8cb9ccc7d9a8b8bdef1187
Greg Bolet <gbolet@vt.edu>
2024-12-23 23:44:40 -0500

    got to 100% build completion, need to add in other omp codes and clean up CMake

--- runBuild.sh
+++ runBuild.sh
@@ -8,6 +8,8 @@ cd ./build
 #CXX_FLAGS="-v -I/usr/local/cuda-12.6/targets/x86_64-linux/include -I/usr/local/cuda-12.6/targets/x86_64-linux/include/cuda/std/__cuda -O3"
 CXX_FLAGS="-I/usr/local/cuda-12.6/targets/x86_64-linux/include -L/usr/local/cuda-12.6/targets/x86_64-linux/lib -O3"
 
+# now sure why clang isn't automatically linking openmp, so we include some extra flags
+
 cmake -DCMAKE_C_COMPILER=clang \
       -DCMAKE_CXX_COMPILER=clang++ \
       -DCMAKE_CUDA_HOST_COMPILER=clang++ \
@@ -19,13 +21,16 @@ cmake -DCMAKE_C_COMPILER=clang \
       -DCMAKE_CXX_FLAGS="${CXX_FLAGS} -v" \
       -DCMAKE_CUDA_FLAGS="${CXX_FLAGS} -v" \
       -DCMAKE_EXE_LINKER_FLAGS="-v" \
-      -DOMP_INCLUDE_DIR="/usr/lib/gcc/x86_64-linux-gnu/13/include" \
-      -DCUDA_THRUST_INCLUDE_DIR="/usr/local/cuda-12.6/targets/x86_64-linux/include/thrust" \
-      -DBOOST_INCLUDE_DIR="/usr/include/boost" \
+      -DOMP_INCLUDE_DIR="/usr/lib/llvm-18/lib/clang/18/include" \
+      -DOMP_LINKER_FLAGS="-L/usr/lib/llvm-18/lib;-lgomp" \
       -S../ -B./
 
+#make -j1 VERBOSE=1 streamUM-cuda
 make -j14 VERBOSE=1 all
 
 cd ..
 
+      #-DOMP_INCLUDE_DIR="/usr/lib/gcc/x86_64-linux-gnu/13/include" \
+      #-DCUDA_THRUST_INCLUDE_DIR="/usr/local/cuda-12.6/targets/x86_64-linux/include/thrust" \
+      #-DBOOST_INCLUDE_DIR="/usr/include/boost" \
       #-DCMAKE_CUDA_COMPILER=clang++ \
